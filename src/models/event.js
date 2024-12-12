ca31039ab796171fb0d0eaedb019847caa356d0f
Aswin-optiblack <aswin@optiblack.com>
2024-12-12 19:32:08 +0530

    add api for event creation

--- src/models/event.js
+++ src/models/event.js
@@ -4,6 +4,7 @@ const EventSchema = new Schema({
   eventName: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
   items: [{ type: Schema.Types.ObjectId, ref: 'Item' }], 
+  additionalInfo: { type: Schema.Types.Mixed },
 });
 
 const Event = models.Event || model('Event', EventSchema);
--- src/models/item.js
+++ src/models/item.js
@@ -1,9 +1,8 @@
 import { Schema, model, models } from 'mongoose';
 
 const ItemSchema = new Schema({
-  name: { type: String, required: true },
+  property: { type: String, required: true },
   value: { type: Schema.Types.Mixed, required: true },
-  additionalInfo: { type: Schema.Types.Mixed },
 });
 
 const Item = models.Item || model('Item', ItemSchema);
